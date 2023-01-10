41b4801eccd928df08463099de274c906a6ac3c0
futured_it <73485120+futured-it@users.noreply.github.com>
2023-01-10 22:26:28 +0500

    chore: Basic discord.js runtime things added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,11 @@
+const { Client, Events, GatewayIntentBits } = require('discord.js');
 require('dotenv').config()
+
 const token = process.env.TOKEN
-console.log(token)
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}.`);
+});
+
+client.login(token);
