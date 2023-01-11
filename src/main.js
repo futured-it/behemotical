b05543767d9819c9ac61e57d145615db6d0f9099
futured_it <73485120+futured-it@users.noreply.github.com>
2023-01-12 00:13:06 +0500

    feat: Hippo images sending added

--- src/main.js
+++ src/main.js
@@ -1,11 +1,31 @@
 const { Client, Events, GatewayIntentBits } = require('discord.js');
-require('dotenv').config()
+require('dotenv').config();
+var Scraper = require('images-scraper');
 
-const token = process.env.TOKEN
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+const google = new Scraper({
+  puppeteer: {
+    headless: false,
+  },
+});
+const token = process.env.TOKEN;
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent ] });
+var imagesRes;
 
 client.once(Events.ClientReady, c => {
-	console.log(`Ready! Logged in as ${c.user.tag}.`);
+	console.log(`Ready! Logged in as ${c.user.tag}.`);  
 });
 
-client.login(token);
+client.on('messageCreate', (message) => {
+	if (message.author.id == client.user.id) return
+	if (message.content == "behemoth" || message.content == "hippopotamus" || message.content == "🦛") {
+		message.reply(imagesRes[Math.floor(Math.random() * 200)].url)
+	}
+	if (message.content == "behemoth->servericon") {
+		message.guild.setIcon(imagesRes[Math.floor(Math.random() * 200)].url)
+	}
+});
+(async () => {
+	const results = await google.scrape('hippo', 200);
+	imagesRes = results;
+	client.login(token);
+})();	
